edd48d9326381862396899eea4e83d99e2f3384d
Ermuxan <ermuxan747@gmail.com>
2025-09-05 15:16:44 +0300

    fix "Invalid initData signature"

--- lib/telegram.ts
+++ lib/telegram.ts
@@ -1,7 +1,19 @@
 export const getInitData = () => {
-  if (typeof window !== "undefined" && window.Telegram.WebApp) {
-    return window.Telegram.WebApp.initData;
+  if (typeof window === "undefined") return "";
+
+  const tg = (window as any).Telegram?.WebApp;
+  if (!tg) {
+    console.warn("Telegram WebApp mavjud emas");
+    return "";
   }
 
-  return null;
+  tg.ready();
+
+  const initData = tg.initData;
+  if (!initData) {
+    console.warn("InitData bo‘sh chiqdi");
+    return "";
+  }
+
+  return initData;
 };
